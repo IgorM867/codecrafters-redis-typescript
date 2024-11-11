8d6c45a9585d0bcd7aa380c22c5c2b05274c9394
IgorM867 <imarkiewicz915@gmail.com>
2024-11-11 07:56:20 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,30 @@
 import * as net from "net";
 import { parse } from "./parseRedisCommand";
-import { serialazeBulkString, serialazeSimpleString } from "./serialazeRedisCommand";
+import { serialazeBulkString, serialazeSimpleError, serialazeSimpleString } from "./serialazeRedisCommand";
+
+const storage = new Map();
 
 const commands = {
   PING: () => serialazeSimpleString("PONG"),
   ECHO: (arg: string) => serialazeBulkString(arg),
+  SET: (key: string | undefined, value: string | undefined) => {
+    if (!key) {
+      return serialazeSimpleError("SYNTAX ERR expecting key");
+    }
+    if (!value) {
+      return serialazeSimpleError("SYNTAX ERR expecting value after key");
+    }
+
+    storage.set(key, value);
+    return serialazeSimpleString("OK");
+  },
+  GET: (key: string | undefined) => {
+    if (!key || !storage.has(key)) {
+      return serialazeBulkString("");
+    }
+
+    return serialazeBulkString(storage.get(key));
+  },
 };
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
@@ -18,11 +38,18 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
       case "ECHO":
         connection.write(commands.ECHO(input[1] as string));
         break;
+      case "SET":
+        connection.write(commands.SET(input[1] as string | undefined, input[2] as string | undefined));
+        break;
+      case "GET":
+        connection.write(commands.GET(input[1] as string | undefined));
+        break;
       default:
-        //TODO serialaze REDIS error
-        throw Error(`Unknown command: ${input[0]}`);
+        connection.write(serialazeSimpleError(`Unknown command: ${input[0]}`));
     }
   });
 });
 
 server.listen(6379, "127.0.0.1");
+
+console.log("Redis server is working...");
--- app/serialazeRedisCommand.ts
+++ app/serialazeRedisCommand.ts
@@ -1,4 +1,10 @@
 export function serialazeBulkString(value: string) {
+  if (value.length === 0) {
+    const str = `$${value.length - 1}\r\n${value}\r\n`;
+
+    return new Uint8Array(new Buffer(str));
+  }
+
   const str = `$${value.length}\r\n${value}\r\n`;
 
   return new Uint8Array(new Buffer(str));
@@ -6,6 +12,12 @@ export function serialazeBulkString(value: string) {
 
 export function serialazeSimpleString(value: string) {
   const str = `+${value}\r\n`;
+
+  return new Uint8Array(new Buffer(str));
+}
+
+export function serialazeSimpleError(value: string) {
+  const str = `-${value}\r\n`;
 
   return new Uint8Array(new Buffer(str));
 }
