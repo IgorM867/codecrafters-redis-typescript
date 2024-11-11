96c1082b1658e37a3f9f0b4ffbc127c2a4d48731
IgorM867 <imarkiewicz915@gmail.com>
2024-11-11 08:16:47 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import * as net from "net";
-import { parse } from "./parseRedisCommand";
+import { parse, type RESPDataType } from "./parseRedisCommand";
 import { serialazeBulkString, serialazeSimpleError, serialazeSimpleString } from "./serialazeRedisCommand";
 
 const storage = new Map();
@@ -7,29 +7,41 @@ const storage = new Map();
 const commands = {
   PING: () => serialazeSimpleString("PONG"),
   ECHO: (arg: string) => serialazeBulkString(arg),
-  SET: (key: string | undefined, value: string | undefined) => {
+  SET: (...args: Array<string | undefined>) => {
+    const key = args[0];
+    const value = args[1];
     if (!key) {
       return serialazeSimpleError("SYNTAX ERR expecting key");
     }
     if (!value) {
       return serialazeSimpleError("SYNTAX ERR expecting value after key");
     }
+    if (args[2] && args[2].toUpperCase() === "PX") {
+      if (!args[3]) return serialazeSimpleString("SYNTAX ERR expecting expiry time after PX");
 
-    storage.set(key, value);
+      storage.set(key, { val: value, expriry: Date.now() + Number(args[1]) });
+    }
+
+    storage.set(key, { val: value, expriry: null });
     return serialazeSimpleString("OK");
   },
   GET: (key: string | undefined) => {
     if (!key || !storage.has(key)) {
       return serialazeBulkString("");
     }
+    const { val, expiry } = storage.get(key);
 
-    return serialazeBulkString(storage.get(key));
+    if (expiry && expiry - Date.now() <= 0) {
+      return serialazeBulkString("");
+    }
+
+    return serialazeBulkString(val);
   },
 };
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", (data) => {
-    const [input] = parse(data);
+    const [input]: [RESPDataType[], number] = parse(data) as [RESPDataType[], number];
 
     switch ((input[0] as string).toUpperCase()) {
       case "PING":
@@ -39,7 +51,7 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
         connection.write(commands.ECHO(input[1] as string));
         break;
       case "SET":
-        connection.write(commands.SET(input[1] as string | undefined, input[2] as string | undefined));
+        connection.write(commands.SET(...(input as Array<string | undefined>)));
         break;
       case "GET":
         connection.write(commands.GET(input[1] as string | undefined));
